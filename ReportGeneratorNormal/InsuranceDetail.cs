6e30ce9f51339e0ae259609cc60f347f37481219
sagar <sagar.pathapelli@gmail.com>
2018-03-12 22:49:44 +0530

    5076: Add Insurance Details on Lab Requsition Reports

---
+++ ReportGeneratorNormal/InsuranceDetail.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Molecular.DataAccess.ReportsService
+{
+    public class InsuranceDetail
+    {
+        public string InsuredName { get; set; }
+        public string RelationshipToPatient { get; set; }
+        public string SocialSecurityNumber { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string Zip { get; set; }
+        public string StateName { get; set; }
+
+        public string TypeOfInsurance { get; set; }
+        public string EffectiveDate { get; set; }
+        public string RelationshipToSubscriber { get; set; }
+        public string SubscriberName { get; set; }
+        public string Policyid { get; set; }
+        public string InsuranceCompanyName { get; set; }
+        public string PlanType { get; set; }
+        public string InsurancePhone { get; set; }
+        public string InsuranceAddress { get; set; }
+        public string InsuranceCity { get; set; }
+        public string InsuranceZip { get; set; }
+        public string InsuranceState { get; set; }
+    }
+}
