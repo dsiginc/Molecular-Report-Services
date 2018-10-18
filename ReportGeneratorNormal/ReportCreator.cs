fe9330bae3ee595e4bc5eae30ae1cc77277952e5
RajniSingh14 <Rajnisingh14@gmail.com>
2018-10-17 21:16:06 -0700

    Added TokenGenerator which will be used by ITox to get the PowerBi reports

--- ReportGeneratorNormal/ReportCreator.cs
+++ ReportGeneratorNormal/ReportCreator.cs
@@ -1,6 +1,7 @@
 ﻿using Molecular.DataAccess.AccessionService.Objects;
 using Molecular.DataAccess.ReportsService;
 using Molecular.DataAccess.ToxicologyAccessionService;
+
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -11,6 +12,7 @@ using System.Xml;
 using Telerik.Reporting;
 using Telerik.Reporting.Processing;
 using Telerik.Reporting.XmlSerialization;
+
 
 namespace ReportGenerator
 {
@@ -429,5 +431,16 @@ namespace ReportGenerator
 
             return Convert.ToBase64String(result.DocumentBytes);
         }
+
+        //public async Task<string> GetAccessToken()
+        //{
+        //    UserPasswordCredential creds = new UserPasswordCredential("Vanaja.b@dsiginc.com", "Welcome@123");
+        //    AuthenticationResult authenticationResult = null;
+        //    var authenticationContext = new AuthenticationContext("https://login.windows.net/common/oauth2/token/");
+
+        //    authenticationResult = await authenticationContext.AcquireTokenAsync("https://analysis.windows.net/powerbi/api", "c565eea1-4938-468e-8d5f-35b74ac5e8e5", creds);// new ClientCredential(Secrets.ClientID,Secrets.ClientSecret));
+        //    return authenticationResult.AccessToken.ToString();
+
+        //}
     }
 }
