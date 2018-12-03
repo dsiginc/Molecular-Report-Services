4db7db05bc58757eb30f9865c269147daed2550d
sagar <sagar.pathapelli@gmail.com>
2018-12-03 23:29:21 +0530

    a

--- ReportGeneratorNormal/Utility.cs
+++ ReportGeneratorNormal/Utility.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using ReportGenerator;
+using System.Collections.Generic;
 using System.IO;
 
 namespace ReportGeneratorNormal
@@ -13,6 +14,21 @@ namespace ReportGeneratorNormal
                 item = File.ReadAllBytes(System.Configuration.ConfigurationManager.AppSettings["reportIconsFolder"].ToString() + "\\not_ordered_icon.png");
             }
             return item;
+        }
+        public static bool IsAdditionalTestsExist(List<CTR_ClinicalTestResult> testResults)
+        {
+            bool additionalTestsExist = false;
+            if (testResults != null)
+            {
+                foreach(var item in testResults)
+                {
+                    if (item.ApprovedPanel == false)
+                    {
+                        additionalTestsExist = true;break;
+                    }
+                }
+            }
+            return additionalTestsExist;
         }
     }
 }
